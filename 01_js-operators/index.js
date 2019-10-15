38578a5a5dc3e1ef0bb2e0d27d478c27161ec76e
Simon Sharville <git@simonsharville.co.uk>
2019-10-15 14:33:27 +0100

    Rename Basics folder

---
+++ 01_js-operators/index.js
@@ -0,0 +1,77 @@
+// let name = 'Mosh'; // Variable set to string 'Mosh'
+// console.log(name);
+
+// // Variable names cannot be a reserved keyword.
+// //  Should be meaningful names.
+// // Cannot start with a number.
+// // Cannot contain a space or hyphen (-).
+// // Variable Names are case-sensitive.
+// // 
+
+// -----------------------------------------------------------------------
+// // 01 Variables
+// let firstName = 'Mosh';
+// let lastName = 'Hamedani';
+
+// -----------------------------------------------------------------------
+// // 02 Consants
+// let interestRate = 0.3;
+// interestRate = 1;
+// console.log(interestRate);
+
+// -----------------------------------------------------------------------
+// // 03 Primitive/Value Types
+// let name = 'Mosh';          // String Literal
+// let age = 30;             // Number Literal
+// let isApproved = true;      // Boolean Literal
+// let firstName = undefined;  // Undefined
+// let selectedColor = null;   // Null
+
+// -----------------------------------------------------------------------
+// 04 Dynamic Typing
+
+// -----------------------------------------------------------------------
+// // 05 Objects (Reference Type)
+// // let name = 'mosh';
+// // let age = 30;
+
+// let person = {          // {} = Objet Literal
+//   name: 'Mosh',         // Key or Property
+//   age: 30
+// };
+// // Dot Notation
+// person.name = 'John';
+
+// // Bracket Notation
+// let selection = 'name';
+// person[selection] = 'Mary';
+// console.log(person);
+
+// -----------------------------------------------------------------------
+// // 06 Arrays
+// //  An array is a list of objects
+
+// let selectedColors = ['red', 'blue'];
+// selectedColors[2] = 1;          // Array Literal,  []= Empty Array
+// console.log(selectedColors.length);
+
+// -----------------------------------------------------------------------
+// // 07 Functions
+// // Performs Task or Calculates Value
+
+// Performing a Task
+// function greet(name, lastName) {    // name = peramiter
+//   console.log('Hello ' + name + ' ' + lastName);
+// }
+
+// greet('John', 'Smith'); // John = Argument (The value of the Peramiter)
+// greet('Mary');
+
+
+// 08 Types of Functions
+
+// Calculating a Value
+ function square(number){
+   return number * number;
+ }
+console.log(square(42));
